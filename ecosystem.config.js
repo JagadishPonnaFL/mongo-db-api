fbb3c5eb744cb1471d7f8f36235065350a0a7daa
JagadishPonnaFL <pjagadishfl@gmail.com>
2025-10-09 02:32:36 +0530

    fix: update log file paths in ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,8 +8,8 @@ module.exports = {
       env_production: {
         NODE_ENV: "production"
       },
-      out_file: "/data/logs/api-out.log",
-      error_file: "/data/logs/api-err.log",
+      out_file: "./logs/api-out.log",
+      error_file: "./logs/api-err.log",
       log_date_format: "YYYY-MM-DD HH:mm Z"
     }
   ]
