b01dd29ecc0b3e3f3ce0a73a7035835536c92210
JagadishPonnaFL <pjagadishfl@gmail.com>
2025-10-18 17:23:39 +0530

    scheduler time set to 10 pm dalily

--- services/schedular.js
+++ services/schedular.js
@@ -15,7 +15,7 @@ async function sendDailyMessage() {
 }
 
 // Schedule: second, minute, hour, day of month, month, day of week
- cron.schedule("16 17 * * *", async () => {
+ cron.schedule("0 22 * * *", async () => {
   await sendDailyMessage();
 }, {
   timezone: "Asia/Kolkata" // Set timezone if needed
